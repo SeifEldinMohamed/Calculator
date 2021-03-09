7ccb955a858963c3939a5fed4b4f81043649d253
seif <seifeldinmohamed101@gmail.com>
2021-03-09 17:26:03 +0200

    Caculator App

--- app/src/main/java/com/seif/calculater/splash.kt
+++ app/src/main/java/com/seif/calculater/splash.kt
@@ -6,9 +6,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class splash : AppCompatActivity() {
-    val splashTime = 1000L
+    val splashTime = 1500L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.requestFeature(Window.FEATURE_NO_TITLE)
@@ -18,6 +19,8 @@ class splash : AppCompatActivity() {
         )
         setContentView(R.layout.activity_splash)
 
+        txt_Company.alpha = 0f
+        txt_Company.animate().setDuration(1500).alpha(1.5f)
 
         Handler().postDelayed({
             val home = Intent(this@splash, MainActivity::class.java)
